24d74ab89518a3ee9be752076c3293478da61d59
mahdi keshavarz <69865082+ma-pk1996@users.noreply.github.com>
2024-01-08 14:40:27 +0330

    working on forecast

--- checkweather/src/components/forecast/Forecast.jsx
+++ checkweather/src/components/forecast/Forecast.jsx
@@ -1,22 +1,55 @@
-
-import classes from "./Forecast.module.css"
-
-
+import classes from "./Forecast.module.css";
+import { useState } from "react";
+import { API_KEY } from "../../process.env";
+import axios from "axios";
 export function Forecast() {
-    return (
-        <div className={classes.container}>
-            <div className={classes.content}>
-                <p className={classes.name}>Tehran</p>
-                <p className={classes.temprature}>Temprature 33 </p>
-                <p className={classes.feels}>Feels Like 37</p>
-                <p className={classes.windSpeed}>Wind Speed 12km</p>
-                <p className={classes.humidity}>Humidity 33</p>
-                <p className={classes.tomorrow}>Tomorrow 32</p>
-                <button className={classes.save}>+ Save</button>
-                <div className={classes.map}>
-                </div>
-            </div>
-            <input className={classes.input} placeholder="Search Wherever You Like"/>
-        </div>
-    )
+  const [data, setData] = useState({});
+  const [cityName, setCityName] = useState("Tehran");
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${API_KEY}`;
+  function searchInputHandler(event) {
+    if (event.key === "Enter") {
+      axios
+        .get(url)
+        .then((res) => {
+          setData(res.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+
+      setCityName("");
+    }
+  }
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.content}>
+        <p className={classes.name}>{data ? data.name : ""}</p>
+        <p className={classes.status}>
+          Mood: {data.weather ? data.weather[0].main : ""}
+        </p>
+        <p className={classes.temprature}>
+          Temprature {data.main ? data.main.temp : ""}
+        </p>
+        <p className={classes.feels}>
+          Feels Like {data.main ? data.main.feels_like : ""}
+        </p>
+        <p className={classes.windSpeed}>
+          Wind Speed {data.win ? data.wind.speed : 12}km
+        </p>
+        <p className={classes.humidity}>
+          Humidity {data.main ? data.main.humidity : 32}
+        </p>
+        <button className={classes.save}>+ Save</button>
+        <div className={classes.map}></div>
+      </div>
+      <input
+        value={cityName}
+        onKeyPress={searchInputHandler}
+        onChange={(event) => setCityName(event.target.value)}
+        className={classes.input}
+        placeholder="Enter Wherever You Like"
+      />
+    </div>
+  );
 }
---
+++ checkweather/src/process.env.js
@@ -0,0 +1,15 @@
+
+export const API_KEY = "'6c54487bb31d839cac4a1bdd8175927f'".replace(/'/g, '');
+
+export const processEnv = {
+    REACT_APP_SERVER_URL: 'http://localhost:3004/',
+}
+
+// if(data && data.weather && data.main && data.wind) {
+//     name = data.name;
+//     mood = data.weather[0].main;
+//     tem = ((data.main.temp) / 30).toPrecision(2);
+//     feels = ((data.main.feels_like) / 30).toPrecision(2);
+//     wind = ((data.wind.speed) * 1.6).toPrecision(2);
+//     humidity = data.main.humidity;
+// }
